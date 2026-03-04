fb2bf2548405bbc1bfb8407a8e3a11268d1c7916
nika0-0-11 <nika_gvozdeva0.0@mail.ru>
2026-03-04 09:40:04 +0300

    Step 5: Basic UI completed

---
+++ app/src/main/java/com/gvodeva_deushev/student_material_design/data/DataSource.kt
@@ -0,0 +1,56 @@
+package com.gvodeva_deushev.student_material_design.data
+
+import com.gvodeva_deushev.student_material_design.R
+
+val students = listOf(
+    Student(
+        R.drawable.student_1,
+        R.string.student1, 27,
+        R.string.student1_desc
+    ),
+    Student(
+        R.drawable.student_2,
+        R.string.student2, 29,
+        R.string.student2_desc
+    ),
+    Student(
+        R.drawable.student_3,
+        R.string.student3, 24,
+        R.string.student3_desc
+    ),
+    Student(
+        R.drawable.student_4,
+        R.string.student4, 26,
+        R.string.student4_desc
+    ),
+    Student(
+        R.drawable.student_5,
+        R.string.student5, 2000,
+        R.string.student5_desc
+    ),
+    Student(
+        R.drawable.student_6,
+        R.string.student6, 9,
+        R.string.student6_desc
+    ),
+    Student(
+        R.drawable.student_7,
+        R.string.student7, 8000,
+        R.string.student7_desc
+    ),
+    Student(
+        R.drawable.student_8,
+        R.string.student8, 31,
+        R.string.student8_desc
+    ),
+    Student(
+        R.drawable.student_9,
+        R.string.student9, 41,
+        R.string.student9_desc
+    ),
+    Student(
+        R.drawable.student_10,
+        R.string.student10, 35,
+        R.string.student10_desc
+    ),
+)
